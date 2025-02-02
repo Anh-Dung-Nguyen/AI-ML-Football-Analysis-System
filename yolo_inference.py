f1ae17da0b64276a4cc32bc9f5e53846e109f3d0
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-02-02 23:00:44 +0100

    Nothing change

--- yolo_inference.py
+++ yolo_inference.py
@@ -4,6 +4,6 @@ model = YOLO('yolov8x')
 
 results = model.predict('input_video/08fd33_4.mp4', save = True)
 print(results[0])
-print("====================================")
+print("=====================================")
 for box in results[0].boxes:
     print(box)
