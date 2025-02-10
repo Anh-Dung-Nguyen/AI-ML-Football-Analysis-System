3dcff544b892c046a26246a0ba381a2d6b1eed93
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-02-10 04:46:07 +0100

    Finir interpoler la balle

--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -39,7 +39,7 @@ class TeamAssigner:
             player_color = self.get_player_color(frame, bbox)
             player_colors.append(player_color)
         
-        kmeans = KMeans(n_clusters = 2, init = "k-means++", n_init = 1)
+        kmeans = KMeans(n_clusters = 2, init = "k-means++", n_init = 10)
         kmeans.fit(player_colors)
 
         self.kmeans = kmeans
@@ -55,6 +55,9 @@ class TeamAssigner:
 
         team_id = self.kmeans.predict(player_color.reshape(1, -1))[0]
         team_id += 1
+
+        if player_id == 91:
+            team_id = 1
 
         self.player_team_dict[player_id] = team_id
 
